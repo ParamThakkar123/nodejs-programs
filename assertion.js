bdd3c4519ab88873344319a65158ece544845fa7
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-05-20 15:16:04 +0530

    Create assertion.js
    This program is 3 liner which evaluates the use of the assert() function in the assert module

---
+++ assertion.js
@@ -0,0 +1,3 @@
+var assert = require('assert') //imports the assert module in nodejs
+assert(50 > 70) //throws an assertion error saying that assertions has been evaluated to a falsy value
+assert(50 < 70) // returns true i.e. throws no errors
